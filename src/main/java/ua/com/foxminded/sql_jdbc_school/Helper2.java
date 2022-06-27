32ecfd460115171b1af79c5d7754552acbf7ba9f
Alexander <2951493@gamil.com>
2022-06-27 14:49:05 +0300

    The JdbcGroupDAOTest class is ready.

---
+++ src/main/java/ua/com/foxminded/sql_jdbc_school/Helper2.java
@@ -0,0 +1,14 @@
+package ua.com.foxminded.sql_jdbc_school;
+
+import java.io.IOException;
+
+public class Helper2 {
+
+    public static void main(String[] args) throws IOException {
+        
+        Helper helper = new Helper();
+        helper.print();
+
+    }
+
+}
--- src/test/java/ua/com/foxminded/sql_jdbs_school/dao/jdbc/JdbcGroupDAOTest.java
+++ src/test/java/ua/com/foxminded/sql_jdbs_school/dao/jdbc/JdbcGroupDAOTest.java
@@ -3,14 +3,14 @@ package ua.com.foxminded.sql_jdbs_school.dao.jdbc;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 
-import java.io.FileInputStream;
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -39,46 +39,50 @@ class JdbcGroupDAOTest {
     private static final String GET_ISERTED_GROUP = "selectInsertedGroup";
     private static final String GROUP_NAME = "group_name";
     private static final String NAME_OF_GROUP = "test";
-    private static final String TEST_GROUP_QUERIES_PATH = "D:/repository/SqlJdbcSchool/src/main/"
-            + "resource/test-group-queries.properties";
-    private static final String GROUP_QUERIES_PATH = "D:/repository/SqlJdbcSchool/src/main/"
-            + "resource/group-queries.properties";
-    private static final String TEST_DATA_PATH = "D:/repository/SqlJdbcSchool/src/main/resource/test-data.sql";
+    private static final String TEST_DATA = "test-data.sql";
+    private static final String TEST_DB_PROPERTIES = "test-db.properties";
+    private static final String TEST_QUERIES = "test-queries.properties";
+    private static final String TEST_SCHEMA = "test-schema.sql";
     private static final String END_LINE = "\n";
-    private static final String TEST_TABLES_SCRIPT_PATH = "D:/repository/SqlJdbcSchool/"
-            + "src/main/resource/test-schema.sql";
-    private static final String TEST_DB_PROP_PATH = "D:/repository/SqlJdbcSchool/"
-            + "src/main/resource/test-db.properties";
     private static final String DB_URL = "databaseURL";
     private static final String USER_NAME = "databaseUser";
     private static final String USER_PASS = "databaseUserPassword";
+    private static final boolean RESULTSET_IS_EMPTY = false;
 
     @InjectMocks
-    JdbcGroupDAO universityGroupDao;
+    JdbcGroupDAO jdbcGroupDao;
 
     @Mock
     DAOConnectionPool daoConnectionPoolMock;
 
     @BeforeEach
     void init() throws FileNotFoundException, SQLException, IOException {
+        
 
-        try (FileInputStream testDbInput = new FileInputStream(TEST_DB_PROP_PATH);) {
+        try (InputStream testDbProperitesIn = Thread.currentThread()
+                                             .getContextClassLoader()
+                                             .getResourceAsStream(TEST_DB_PROPERTIES)) {
 
             Properties testDbPoperites = new Properties();
-            testDbPoperites.load(testDbInput);
+            testDbPoperites.load(testDbProperitesIn);
 
-            try (Connection con = DriverManager.getConnection(testDbPoperites.getProperty(DB_URL),
+            try (InputStream schemaInput = Thread.currentThread()
+                                                 .getContextClassLoader()
+                                                 .getResourceAsStream(TEST_SCHEMA);
+                 InputStream testDataInput = Thread.currentThread()
+                                                   .getContextClassLoader()
+                                                   .getResourceAsStream(TEST_DATA);
+                 Connection con = DriverManager.getConnection(testDbPoperites.getProperty(DB_URL),
                                                               testDbPoperites.getProperty(USER_NAME), 
                                                               testDbPoperites.getProperty(USER_PASS));
                  Statement statement = con.createStatement();) {
-
-                Path tableScriptPath = Paths.get(TEST_TABLES_SCRIPT_PATH);
-                String tablesScript = Files.lines(tableScriptPath).collect(Collectors.joining(END_LINE));
-                statement.execute(tablesScript);
-
-                Path testDataScriptPath = Paths.get(TEST_DATA_PATH);
-                String testDataScrip = Files.lines(testDataScriptPath).collect(Collectors.joining(END_LINE));
-                statement.execute(testDataScrip);
+                
+                String schema = new BufferedReader(new InputStreamReader(schemaInput)).lines()
+                        .collect(Collectors.joining(END_LINE));
+                String testData = new BufferedReader(new InputStreamReader(testDataInput)).lines()
+                        .collect(Collectors.joining(END_LINE));
+                statement.execute(schema);
+                statement.execute(testData);
             }
         }
     }
@@ -87,18 +91,20 @@ class JdbcGroupDAOTest {
     void readGroupsWithLessOrEqualStudents_AcceptableArg_RightStudentQuantity()
             throws FileNotFoundException, IOException, SQLException, DAOException {
 
-        try (FileInputStream testDb = new FileInputStream(TEST_DB_PROP_PATH);
-             FileInputStream groupQueries = new FileInputStream(GROUP_QUERIES_PATH);) {
+        try (InputStream testDbPropertiesIn = this.getClass()
+                                                  .getClassLoader()
+                                                  .getResourceAsStream(TEST_DB_PROPERTIES);) {
 
-            Properties properties = new Properties();
-            properties.load(testDb);
+            Properties dbProp = new Properties();
+            dbProp.load(testDbPropertiesIn);
 
-            try (Connection con = DriverManager.getConnection(properties.getProperty(DB_URL),
-                 properties.getProperty(USER_NAME), properties.getProperty(USER_PASS));) {
+            try (Connection con = DriverManager.getConnection(dbProp.getProperty(DB_URL),
+                                                              dbProp.getProperty(USER_NAME), 
+                                                              dbProp.getProperty(USER_PASS));) {
 
                 when(daoConnectionPoolMock.getConnection()).thenReturn(con);
                 assertEquals(GROUP_QUANITY,
-                             universityGroupDao.getGroupsHavingLessOrEqualStudents(STUDENTS_QUANTITY)
+                             jdbcGroupDao.getGroupsHavingLessOrEqualStudents(STUDENTS_QUANTITY)
                                                .size());
             }
         }
@@ -110,16 +116,18 @@ class JdbcGroupDAOTest {
                                                                                   SQLException, 
                                                                                   DAOException {
         
-        try (FileInputStream testDbProperties = new FileInputStream(TEST_DB_PROP_PATH);) {
+        try (InputStream testDbPropertiesIn = this.getClass().getClassLoader()
+                                                             .getResourceAsStream(TEST_DB_PROPERTIES);) {
 
             Properties properties = new Properties();
-            properties.load(testDbProperties);
+            properties.load(testDbPropertiesIn);
 
             try (Connection con = DriverManager.getConnection(properties.getProperty(DB_URL),
-                    properties.getProperty(USER_NAME), properties.getProperty(USER_PASS));) {
+                                                              properties.getProperty(USER_NAME), 
+                                                              properties.getProperty(USER_PASS));) {
 
                 when(daoConnectionPoolMock.getConnection()).thenReturn(con);
-                assertEquals(TOTAL_GROUP_QUANTITY, universityGroupDao.getAll().size());
+                assertEquals(TOTAL_GROUP_QUANTITY, jdbcGroupDao.getAll().size());
             }
         }
     }
@@ -130,13 +138,14 @@ class JdbcGroupDAOTest {
                                                                 SQLException, 
                                                                 DAOException {
 
-        try (FileInputStream testDbProperites = new FileInputStream(TEST_DB_PROP_PATH);
-             FileInputStream groupQueries = new FileInputStream(GROUP_QUERIES_PATH);
-             FileInputStream testGroupQueries = new FileInputStream(TEST_GROUP_QUERIES_PATH);) {
+        try (InputStream testDbPropertiesIn = this.getClass().getClassLoader()
+                                                             .getResourceAsStream(TEST_DB_PROPERTIES);
+             InputStream testQueriesIn = this.getClass().getClassLoader()
+                                                        .getResourceAsStream(TEST_QUERIES);) {
 
             Properties properties = new Properties();
-            properties.load(testDbProperites);
-            properties.load(testGroupQueries);
+            properties.load(testDbPropertiesIn);
+            properties.load(testQueriesIn);
 
             try (Connection con = DriverManager.getConnection(properties.getProperty(DB_URL),
                                                               properties.getProperty(USER_NAME), 
@@ -145,12 +154,12 @@ class JdbcGroupDAOTest {
                 List<GroupEntity> group = new ArrayList<>();
                 group.add(new GroupEntity(NAME_OF_GROUP));
                 when(daoConnectionPoolMock.getConnection()).thenReturn(con);
-                universityGroupDao.insert(group);
+                jdbcGroupDao.insert(group);
                 GroupEntity insertedGroup = null;
                 
                 try (Statement statement = con.createStatement();
-                     ResultSet resultSet = statement.executeQuery(properties
-                             .getProperty(GET_ISERTED_GROUP));) {
+                     ResultSet resultSet = statement.executeQuery(
+                             properties.getProperty(GET_ISERTED_GROUP));) {
                     
                     while(resultSet.next()) {
                         insertedGroup = new GroupEntity(resultSet.getString(GROUP_NAME));
@@ -162,7 +171,29 @@ class JdbcGroupDAOTest {
     }
     
     @Test
-    void deleteAll_DatabaseAllElementsDeletion_DatabaseHasNoElements () {
-        
+    void deleteAll_AllDatabaseElementsDeletion_DatabaseHasNoElements () throws IOException, 
+                                                                               SQLException, 
+                                                                               DAOException {
+        try (InputStream testDbPropertiesIn = this.getClass().getClassLoader()
+                                                             .getResourceAsStream(TEST_DB_PROPERTIES);) {
+            Properties properties = new Properties();
+            properties.load(testDbPropertiesIn);
+            
+            try (Connection con = DriverManager.getConnection(properties.getProperty(DB_URL), 
+                                                              properties.getProperty(USER_NAME), 
+                                                              properties.getProperty(USER_PASS));
+                 PreparedStatement prStatement = con.prepareStatement("select * from department.groups");
+                 InputStream testQueries = this.getClass().getClassLoader()
+                                                          .getResourceAsStream(TEST_QUERIES);) {
+                
+                when(daoConnectionPoolMock.getConnection()).thenReturn(con);
+                jdbcGroupDao.deleteAll();
+                
+                try (ResultSet resultSet = prStatement.executeQuery();) {
+                    
+                    assertEquals(RESULTSET_IS_EMPTY , resultSet.next());
+                }
+            }
+        }
     }
 }
